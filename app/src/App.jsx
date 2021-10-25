08510da7215ae321ee578ecf1e8d6b937f7e1e9c
Claire Devlin <claire.devlin@hotmail.com>
2021-10-25 12:22:24 -0400

    add ResourceList to home page

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,16 +1,25 @@
+import { useState, useEffect } from 'react';
 import logo from './logo.svg';
-import './App.css';
+// import './App.css';
 import HomePage from './components/HomePage.jsx';
 import Navbar from './components/NavBar';
-import { getResources } from "./helpers/selectors"
+import ResourceList from './components/ResourceList';
+// import { getResources } from "./helpers/selectors"
 import useApplicationData from "./hooks/useApplicationData";
 
 export default function App() {
+  const { state } = useApplicationData()
+  // const [resources, setResources] = useState([]);
+
+  useEffect(() => {
+    // const resources = getResources();
+    // setResources(prev => [...prev, ...state.resources])
+    console.log('state.resources', state.resources);
+  }, [])
   return (
     <main className="App">
       <Navbar />
-      <HomePage />
-      <div>Hello</div>
+      <ResourceList resources={state.resources} />
     </main>
   );
 }
