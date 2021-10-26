1391130eb17b84f2e237ccd7a828efb21d6092c8
Claire Devlin <claire.devlin@hotmail.com>
2021-10-25 21:54:49 -0400

    resource view boostrap

--- app/src/components/ResourceView.jsx
+++ app/src/components/ResourceView.jsx
@@ -12,14 +12,13 @@ export default function ResourceView(props) {
   }, [])
 
   return (
-    <section>
-
-      <img src={resource.image} width='300px' />
-      <div>
+    <section className="d-flex flex-row m-5 p-5" style={{ maxHeight: '300px' }}>
+      <img src={resource.image_src} width='300px' />
+      <div className="pl-2" style={{ paddingLeft: '1.5em' }}>
         <h3>{resource.name}</h3>
         <p>{resource.long_description}</p>
+        <a href={resource.website}>Visit the Website!</a>
       </div>
-
     </section>
   )
 }
