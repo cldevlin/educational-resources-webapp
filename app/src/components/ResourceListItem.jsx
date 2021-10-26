f28cde3e476af0930dba42257a59750a34a3f3c7
Claire Devlin <claire.devlin@hotmail.com>
2021-10-25 21:54:22 -0400

    fix thumbnails

--- app/src/components/ResourceListItem.jsx
+++ app/src/components/ResourceListItem.jsx
@@ -1,12 +1,12 @@
-import alloprof from '../assets/images/alloprof.jpg'
+// import alloprof from '../assets/images/alloprof.jpg'
 
 
 export default function ResourceListItem(props) {
   const { image, name, description, slug } = props;
 
   return (
-    <div className="d-flex m-1 p-1" style={{ maxHeight: '300px' }}>
-      <img src={image} width='300px' />
+    <div className="d-flex m-1 p-1" style={{ height: '300px' }}>
+      <img className='img-thumbnail p-0' src={image} width='300px' />
       <div className="pl-2" style={{ paddingLeft: '1.5em' }}>
         <a href={`/resources/${slug}`}>
           <h3>{name}</h3>
