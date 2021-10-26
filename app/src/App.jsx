58d14b7de5f8dfa89ce8a2771c933c12c0e17821
Claire Devlin <claire.devlin@hotmail.com>
2021-10-25 20:48:47 -0400

    Add routing in App.jsx

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,11 +1,15 @@
 import { useState, useEffect } from 'react';
+import { Switch, Route } from "react-router-dom";
 import logo from './logo.svg';
 // import './App.css';
-import HomePage from './components/HomePage.jsx';
+// import HomePage from './components/HomePage.jsx';
 import Navbar from './components/NavBar';
+import Search from './components/Search';
 import ResourceList from './components/ResourceList';
+import ResourceView from './components/ResourceView';
 // import { getResources } from "./helpers/selectors"
 import useApplicationData from "./hooks/useApplicationData";
+import { getResource } from './helpers/selectors';
 
 export default function App() {
   const { state } = useApplicationData()
@@ -18,8 +22,17 @@ export default function App() {
   }, [])
   return (
     <main className="App">
-      <Navbar />
-      <ResourceList resources={state.resources} />
+      <Switch>
+        <Route path='/' exact>
+          <Navbar />
+          <Search />
+          <ResourceList resources={state.resources} />
+        </Route>
+        <Route path='/resources/:slug'>
+          <Navbar />
+          <ResourceView getResource={getResource} state={state} />
+        </Route>
+      </Switch>
     </main>
   );
 }
