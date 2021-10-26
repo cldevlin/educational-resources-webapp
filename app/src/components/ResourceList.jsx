9481024fe8567be28264e6d0dccf28ca9784aaad
Claire Devlin <claire.devlin@hotmail.com>
2021-10-25 20:49:36 -0400

    Resource list styling

--- app/src/components/ResourceList.jsx
+++ app/src/components/ResourceList.jsx
@@ -2,19 +2,20 @@ import ResourceListItem from './ResourceListItem.jsx'
 
 export default function ResourceList(props) {
   const { resources } = props;
-  console.log('resources (ResourceList.jsx', resources);
-  const dayList = resources.map((resourceData) => {
+  // console.log('resources (ResourceList.jsx)', resources);
+  const resourceList = resources.map((resourceData) => {
     return <ResourceListItem
       key={resourceData.id}
-      image={resourceData.image}
+      image={resourceData.image_src}
       name={resourceData.name}
       description={resourceData.description}
+      slug={resourceData.slug}
     />
   });
 
   return (
-    <ul>
-      {dayList}
+    <ul className="border p-2 mx-5 bd-highlight">
+      {resourceList}
     </ul>
   );
 }
--- app/src/components/ResourceListItem.jsx
+++ app/src/components/ResourceListItem.jsx
@@ -1,12 +1,18 @@
+import alloprof from '../assets/images/alloprof.jpg'
+
 
 export default function ResourceListItem(props) {
-  const { image, name, description } = props;
+  const { image, name, description, slug } = props;
 
   return (
-    <div>
-      <img src={image} />
-      <h3>{name}</h3>
-      <p>{description}</p>
+    <div className="d-flex m-1 p-1" style={{ maxHeight: '300px' }}>
+      <img src={image} width='300px' />
+      <div className="pl-2" style={{ paddingLeft: '1.5em' }}>
+        <a href={`/resources/${slug}`}>
+          <h3>{name}</h3>
+        </a>
+        <p>{description}</p>
+      </div>
     </div>
   )
 }
